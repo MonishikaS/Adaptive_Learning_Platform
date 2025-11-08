444984a65d5ff57a086082838bba8664705a0933
MonishikaS <112560668+MonishikaS@users.noreply.github.com>
2025-11-08 15:32:06 +0530

    Create Dashboard.js

---
+++ Frontend/client/src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Student Dashboard</h2>
+      {/* Add mastery, charts, feedback here */}
+    </div>
+  );
+}
+export default Dashboard;
