49f2b3b1d6507fd8ded48c671d4e44dc9c859b7a
MonishikaS <112560668+MonishikaS@users.noreply.github.com>
2025-11-08 15:32:24 +0530

    Create QuizPage.js

---
+++ Frontend/client/src/pages/QuizPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function QuizPage() {
+  return (
+    <div>
+      <h2>Adaptive Quiz</h2>
+      {/* Personalized question flow */}
+    </div>
+  );
+}
+export default QuizPage;
