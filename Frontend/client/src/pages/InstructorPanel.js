24dedc58766b7da512feb4b8513e6aa68d722d3f
MonishikaS <112560668+MonishikaS@users.noreply.github.com>
2025-11-08 15:32:43 +0530

    Create InstructorPanel.js

---
+++ Frontend/client/src/pages/InstructorPanel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InstructorPanel() {
+  return (
+    <div>
+      <h2>Instructor Panel</h2>
+      {/* Manage bank, analytics, rules */}
+    </div>
+  );
+}
+export default InstructorPanel;
